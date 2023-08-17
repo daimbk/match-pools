6fd267c20705952f76e3caeaff73ecdd46e1c866
Daim Bin Khalid <daimbkhalid@gmail.com>
2023-08-17 19:47:02 +0500

    fn: get player names

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 // script to generate match pools
-use std::io;
+use std::io::{self, Write};
 
+
+fn get_player_names(total_players: i32) -> Vec<String> {
+    let mut player_names: Vec<String> = Vec::new();
+    
+    for _ in 0..total_players {
+        print!("Enter player name: ");
+        io::stdout().flush();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        let player_name: String = input.trim().to_string();
+        player_names.push(player_name);
+    }
+
+    return player_names;
+}
 
 fn main() {
-    println!("Total number of players: ");
+    print!("Total number of players: ");
+    io::stdout().flush();
 
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
@@ -13,4 +30,7 @@ fn main() {
     let total_matches: i32 = total_players * (total_players - 1) / 2;
 
     println!("Total number of matches: {}", total_matches);
+
+    // get player names in a list
+    let player_names: Vec<String> = get_player_names(total_players);
 }
