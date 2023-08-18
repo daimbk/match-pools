6c90ba0350838febc0257cf29be928dabebd98fb
Daim Bin Khalid <daimbkhalid@gmail.com>
2023-08-18 21:43:18 +0500

    format: rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 // script to generate match pools
 use std::io::{self, Write};
 
-
 fn get_player_names(total_players: i32) -> Vec<String> {
     let mut player_names: Vec<String> = Vec::new();
-    
+
     for _ in 0..total_players {
         print!("Enter player name: ");
         io::stdout().flush().expect("Failed to flush stdout");
 
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
         let player_name: String = input.trim().to_string();
         player_names.push(player_name);
     }
@@ -23,7 +24,9 @@ fn main() {
     io::stdout().flush().expect("Failed to flush stdout");
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
     let total_players: i32 = input.trim().parse().expect("Invalid input");
 
     // calculate total number of unique matches
